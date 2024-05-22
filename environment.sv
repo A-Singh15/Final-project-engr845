40452f6617a765458148343f4fd8a8230dc38849
Aaron <70713574+A-Singh15@users.noreply.github.com>
2024-05-22 12:30:30 -0700

    Update environment.sv

--- environment.sv
+++ environment.sv
@@ -4,7 +4,25 @@ class Environment;
     Monitor mon;
     Scoreboard sb;
     virtual intf vif;
-    top dut; // Add the DUT instance
+
+    // Instantiate the DUT as a module
+    top dut(
+        .clock(vif.clock),
+        .reset(vif.reset),
+        .start(vif.start),
+        .video_frame_data(vif.video_frame_data),
+        .control_signals(vif.control_signals),
+        .motionX(vif.motionX),
+        .motionY(vif.motionY),
+        .BestDist(vif.BestDist),
+        .completed(vif.completed),
+        .AddressR(vif.AddressR),
+        .AddressS1(vif.AddressS1),
+        .AddressS2(vif.AddressS2),
+        .R(vif.R),
+        .S1(vif.S1),
+        .S2(vif.S2)
+    );
 
     function new(virtual intf vif);
         this.vif = vif;
@@ -12,7 +30,6 @@ class Environment;
         drv = new(vif);
         mon = new(vif);
         sb = new();
-        dut = new();
     endfunction
 
     task run();
