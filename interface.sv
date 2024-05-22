c1e24cd85e62fbc93c6415e366984400058934fb
Aaron <70713574+A-Singh15@users.noreply.github.com>
2024-05-22 11:30:20 -0700

    Update interface.sv

--- interface.sv
+++ interface.sv
@@ -1,10 +1,15 @@
+// interface.sv
 interface intf;
     logic clock;
-    logic reset;
     logic start;
     logic [7:0] video_frame_data;
     logic [7:0] control_signals;
     logic [3:0] motionX, motionY;
     logic [7:0] BestDist;
     logic completed;
+    logic [7:0] AddressR;
+    logic [9:0] AddressS1, AddressS2;
+    logic [7:0] R;
+    logic [7:0] S1;
+    logic [7:0] S2;
 endinterface
