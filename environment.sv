aa7b285957bbd645ddf3f836291cc4b13688e750
Aaron <70713574+A-Singh15@users.noreply.github.com>
2024-05-22 11:24:55 -0700

    Create environment.sv

---
+++ environment.sv
@@ -0,0 +1,23 @@
+class Environment;
+    Generator gen;
+    Driver drv;
+    Monitor mon;
+    Scoreboard sb;
+    virtual intf vif;
+
+    function new(virtual intf vif);
+        this.vif = vif;
+        gen = new(vif);
+        drv = new(vif);
+        mon = new(vif);
+        sb = new();
+    endfunction
+
+    task run();
+        fork
+            gen.generate();
+            drv.drive();
+            mon.monitor();
+        join_none
+    endtask
+endclass
