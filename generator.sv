11c97ce8d7e20128155d515f556d04f333dadef0
Aaron <70713574+A-Singh15@users.noreply.github.com>
2024-05-22 11:23:55 -0700

    Create generator.sv

---
+++ generator.sv
@@ -0,0 +1,20 @@
+class Generator;
+    virtual intf vif;
+
+    function new(virtual intf vif);
+        this.vif = vif;
+    endfunction
+
+    task generate();
+        Transaction tr = new();
+        forever begin
+            if (!$urandom_range(0, 5)) begin
+                assert(tr.randomize()) else $fatal("Randomization failed");
+                vif.video_frame_data = tr.video_frame_data;
+                vif.control_signals = tr.control_signals;
+                tr.display();
+                #10;
+            end
+        end
+    endtask
+endclass
