a9d76bfba7592c047fefb9ea2df621e855f66718
Aaron <70713574+A-Singh15@users.noreply.github.com>
2024-05-22 11:24:31 -0700

    Create monitor.sv

---
+++ monitor.sv
@@ -0,0 +1,14 @@
+class Monitor;
+    virtual intf vif;
+
+    function new(virtual intf vif);
+        this.vif = vif;
+    endfunction
+
+    task monitor();
+        forever begin
+            @(posedge vif.clock);
+            $display("Time: %0t, motionX: %0d, motionY: %0d, BestDist: %0d", $time, vif.motionX, vif.motionY, vif.BestDist);
+        end
+    endtask
+endclass
